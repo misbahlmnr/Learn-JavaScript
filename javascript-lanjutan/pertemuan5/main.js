e4c77e7374664876de87476956db1d4961508c6b
MisbahDev <79269174+misbahdevs@users.noreply.github.com>
2022-09-18 09:04:23 +0700

    Update main.js

--- javascript-lanjutan/pertemuan5/main.js
+++ javascript-lanjutan/pertemuan5/main.js
@@ -30,3 +30,56 @@ let selamatSiang = ucapkanSalam('siang');
 let selamatMalam = ucapkanSalam('malam');
 
 selamatPagi('misbah');
+
+
+// private method
+let counter = 0;
+let add = function() {
+    return ++counter;
+};
+
+console.log(add()); // 1
+console.log(add()); // 2
+console.log(add()); // 3
+
+// ini gaada masalah, cuma pada saat ditengah jalan / dicode line berapa, ada yang menambahkan seperti ini
+let counter = 0;
+let add = function() {
+    return ++counter;
+};
+
+counter = 10;
+
+console.log(add()); // 11
+console.log(add()); // 12
+console.log(add()); // 13
+
+// ini terjadi karena hoisting
+
+// solusinya bisa dengan menyimpan variable counter ke local context function
+let add = function() {
+    let counter = 0;
+    return ++counter;
+};
+
+console.log(add()); // 1
+console.log(add()); // 1
+console.log(add()); // 1
+
+// tapi hasilnya jadi satu semua, karena setiap function di panggil, counter di set jadi 0 + 1 = 1
+
+// oleh sebab itu kita bisa menggunakan closure
+let counter = 0;
+let add = function() {
+    return function() {
+       return ++counter;
+    }
+};
+
+let a = add();
+
+console.log(a()); // 1
+console.log(a()); // 2
+console.log(a()); // 3
+
+// sekarang sukses, karena counter yang di acu adalah di function parent nya :)
